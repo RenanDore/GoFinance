070ad7a0bf7e722ccbf5267851f5057a92abfebc
Renan Dore <dore.renan@gmail.com>
2022-01-20 11:06:55 -0300

    Criando botões de inserir e excluir o cadastro e as funcionalidades no layout

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/Form/Button";
-
 import { Input } from "../../components/Form/Input";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
-import { Container, Header, Title, Form, Fields } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./styles";
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("");
+
+  function handleTransactionTypesSelect(type: "up" | "down") {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -16,6 +29,21 @@ export function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionTypesSelect("up")}
+              isActive={transactionType === "up"}
+            />
+
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionTypesSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionTypes>
         </Fields>
         <Button title="Enviar" />
       </Form>
